8ca60335cd3850b6e5dc97bd3d6a969e66483f50
Ben <55573017+muranatsuradio@users.noreply.github.com>
2022-03-13 20:45:02 +0800

    01. Movement Change

--- Assets/ShoppingCart/Scripts/PlayerMovement/SprintComponent.cs
+++ Assets/ShoppingCart/Scripts/PlayerMovement/SprintComponent.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class SprintComponent : MonoBehaviour
 {
@@ -14,6 +16,8 @@ public class SprintComponent : MonoBehaviour
     public Transform EndChargingTransform;
 
     public float SprintSpeedMultiplier = 5f;
+    public float FrictionRate = 0.5f;
+    private float _localFrictionRate;
 
     private float _leftHandSprintTimer = 0f;
     private float _rightHandSprintTimer = 0f;
@@ -21,40 +25,105 @@ public class SprintComponent : MonoBehaviour
     private bool _isLeftHandSprint;
     private bool _isRightHandSprint;
 
+    [SerializeField]
+    private XRNode _leftHandButton;
+    [SerializeField]
+    private XRNode _rightHandButton;
+
+    private List<InputDevice> _inputDevices = new List<InputDevice>();
+    private InputDevice _rightHandInputDevice;
+    private InputDevice _leftHandInputDevice;
+
+    private bool _isLeftHandButtonPressed;
+    private bool _isRightHandButtonPressed;
+
+    #region Unity Methods
+    private void OnEnable()
+    {
+        if (!_rightHandInputDevice.isValid || !_leftHandInputDevice.isValid)
+        {
+            GetDevice();
+        }
+    }
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+
+        if (!_rightHandInputDevice.isValid || !_leftHandInputDevice.isValid)
+        {
+            GetDevice();
+        }
+
+        _localFrictionRate = FrictionRate;
+    }
+
+    private void Update()
+    {
+        _leftHandInputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out _isLeftHandButtonPressed);
+        _rightHandInputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out _isRightHandButtonPressed);
+
+        Debug.Log(_isLeftHandButtonPressed || _isRightHandButtonPressed);
     }
 
     private void FixedUpdate()
     {
-        SprintMove(LeftHandGameObject, ref _leftHandSprintTimer, ref _isLeftHandSprint);
-        SprintMove(RightHandGameObject, ref _rightHandSprintTimer, ref _isRightHandSprint);
+        if (!_rigidbody) return;
+        MoveFriction();
+        SprintMove(LeftHandGameObject, _isLeftHandButtonPressed, ref _leftHandSprintTimer, ref _isLeftHandSprint);
+        SprintMove(RightHandGameObject, _isRightHandButtonPressed, ref _rightHandSprintTimer, ref _isRightHandSprint);
     }
+    #endregion
 
-    private void SprintMove(GameObject handObject, ref float sprintTimer, ref bool isSprinting)
+    #region Custom Methods
+    private void SprintMove(GameObject handObject, bool isPressedButton, ref float sprintTimer, ref bool isSprinting)
     {
-        // Arrive Start Position, Always Set timer => 0
-        if (handObject.transform.position.y >= StartChargingTransform.position.y)
+        if (isPressedButton)
         {
-            sprintTimer = 0f;
-            isSprinting = true;
+            // Arrive Start Position, Always Set timer => 0
+            if (handObject.transform.position.y >= StartChargingTransform.position.y)
+            {
+                sprintTimer = 0f;
+                isSprinting = true;
+                _localFrictionRate = 0f;
+            }
         }
-
 
         // Smaller than Start Position, Start Timer
         if (handObject.transform.position.y >= StartChargingTransform.position.y || !isSprinting) return;
 
         sprintTimer += Time.deltaTime;
 
-
         // Arrive End Position, Accelerate Player According To Timer.
         if (handObject.transform.position.y >= EndChargingTransform.position.y) return;
 
+        // Release TriggerButton to Activate Acceleration
+        if (isPressedButton) return;
+
+        // Acceleration Formula: 
         _rigidbody.velocity += ForwardResource.transform.forward * SprintSpeedMultiplier / sprintTimer * Time.fixedDeltaTime;
 
         isSprinting = false;
 
-        Debug.Log("Sprint!!!");
+        _localFrictionRate = FrictionRate;
     }
+
+    private void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(_leftHandButton, _inputDevices);
+        _leftHandInputDevice = _inputDevices.FirstOrDefault();
+        InputDevices.GetDevicesAtXRNode(_rightHandButton, _inputDevices);
+        _rightHandInputDevice = _inputDevices.FirstOrDefault();
+    }
+
+    private void MoveFriction()
+    {
+        Vector3 horizontalSpeed = new Vector3(_rigidbody.velocity.x, 0, _rigidbody.velocity.z);
+        float angle = Vector3.Dot(horizontalSpeed, transform.forward) / horizontalSpeed.magnitude;
+
+        if (angle <= 0f || horizontalSpeed.magnitude <= 0.1f) return;
+
+        _rigidbody.AddForce(-transform.forward * _localFrictionRate);
+    }
+    #endregion
 }
--- Assets/ShoppingCart/Scripts/TestScript.cs
+++ Assets/ShoppingCart/Scripts/TestScript.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class TestScript : MonoBehaviour
 {
-    CharacterController _characterController;
-    void Start()
+    [SerializeField]
+    private XRNode _xrNode;
+
+    private List<InputDevice> _inputDevices = new List<InputDevice>();
+
+    private InputDevice _inputDevice;
+
+    private void OnEnable()
     {
-        _characterController = GetComponent<CharacterController>();
+        if (!_inputDevice.isValid)
+        {
+            GetDevice();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        _characterController.Move(transform.forward);
+        if (!_inputDevice.isValid)
+        {
+            GetDevice();
+        }
+
+        bool isTriggerButtonPressed = false;
+
+        if (_inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out isTriggerButtonPressed) && isTriggerButtonPressed)
+        {
+            Debug.Log(isTriggerButtonPressed);
+        }
+
+    }
+
+    private void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(_xrNode, _inputDevices);
+        _inputDevice = _inputDevices.FirstOrDefault();
     }
 }

72b0987948a6b69a8449e8003707cb8bf093071a
Ben <55573017+muranatsuradio@users.noreply.github.com>
2022-04-29 14:05:38 +0800

    118. BGM

---
+++ Assets/ShoppingCart/Scripts/Game Scene/BackgroundSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace ShoppingCart.Scripts.Game_Scene
+{
+    public class BackgroundSystem : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            GameOverSystem.Instance.OnCountdownEnd += StartPlayMusic;
+        }
+
+        private void OnDisable()
+        {
+            GameOverSystem.Instance.OnCountdownEnd -= StartPlayMusic;
+        }
+
+        private void StartPlayMusic()
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
